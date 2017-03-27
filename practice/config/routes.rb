56a964879126dc6c477897cd9ec5aeeeea310b41
SD-PC019 <sd-pc019@SD-PC019-no-MacBook-Pro.local>
2017-03-27 13:21:37 +0900

    /topics/index 作成

--- practice/config/routes.rb
+++ practice/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  # root
+  root 'top#index'
+
+  resources :topics
+
+  get 'top/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
